1b5a81c56478cf608b99112217af80e2e5fcf3f4
Steven Cheng <94231194+steve-cheng78@users.noreply.github.com>
2024-03-15 18:23:07 -0400

    Pre-physical testing, works as expected on laptop

--- Updated control system code/RTC_wifi.py
+++ Updated control system code/RTC_wifi.py
@@ -8,14 +8,13 @@ import ustruct as struct
 
 ssid, password = "Verizon_6FRDHP", "grip9-wordy-con"
 
-# wintertime / Summerzeit
+# Timezone offset from GMT
 GMT_OFFSET = 3600 * 4 # 3600 = 1 h (wintertime)
-#GMT_OFFSET = 3600 * 2 # 3600 = 1 h (summertime)
 
 # NTP-Host
 NTP_HOST = 'pool.ntp.org'
 
-# Funktion: get time from NTP Server
+# Retrieve time from NTP Server
 def getTimeNTP():
     NTP_DELTA = 2208988800
     NTP_QUERY = bytearray(48)
@@ -31,7 +30,7 @@ def getTimeNTP():
     ntp_time = struct.unpack("!I", msg[40:44])[0]
     return time.gmtime(ntp_time - NTP_DELTA - GMT_OFFSET)
 
-# Funktion: copy time to PI pico´s RTC
+# Copy time to PI pico´s RTC
 def setTimeRTC(rtc):
     tm = getTimeNTP()
     rtc.datetime((tm[0], tm[1], tm[2], 0, tm[3], tm[4], tm[5], 0))     #weekday cannot be set
@@ -60,14 +59,9 @@ def connectWIFI():
 
 
 if __name__ == "__main__":
-    connectWIFI()
-    
+    connectWIFI()  
     rtc = RTC()  
-
-    # Zeit setzen
     setTimeRTC(rtc)
-
-    # Aktuelles Datum ausgeben
     print()
     #print(rtc.datetime())                    #yr,mnth,dy,wkdy,hr,min,sec,subsec
     print(time.localtime())                  #yr,mnth,dy,hr,min,sec,wkdy,yrdy --- Still based on RTC
--- Updated control system code/main.py
+++ Updated control system code/main.py
@@ -9,18 +9,20 @@ CW = 1
 CCW = 0
 SPR = 200        #steps per revolution in full step mode
 
+led = Pin("LED", Pin.OUT)
 DIR = Pin(18, Pin.OUT)
 STEP = Pin(17, Pin.OUT)
 #initilialize direction
 DIR.value(CW)
+led.low()
 
 total_rotations = 20
 total_steps = SPR*total_rotations    #200*20 = 4000
 
 #For demo purposes, speeds up daily process by a factor of timeFactor.
 #Example: timeFactor of 4 allows process of 24 hours to complete in 6 hours.
-#Probably want to set time factor of 144 to complete day process in 5 minutes.
-timeFactor = 144
+#Probably want to set time factor of 288 to complete day process in 5 minutes.
+timeFactor = 360
 top_pauseTime = 3600            #stay in top position for 1 hour
 dayStart = 21600             #6 hours (into the day)
 dayEnd = 64800               #18 hours (into the day)
@@ -34,7 +36,7 @@ def calibrateRTC():
     RTC_wifi.setTimeRTC(rtc)      # set time, from RTFC_wifi module
     print()
     print(rtc.datetime())      # display current datetime
-
+    led.high()              #show successful calibration
 
 def rotateMotor(steps, direction, delay, currSteps):
     for x in range(steps):
@@ -62,10 +64,14 @@ def dayProcess(timeFactor, operating_daytime, top_pauseTime, total_steps, daySta
     TFtop_pauseTime = top_pauseTime/timeFactor
     
     #Motor stops at the top for 1 hour, not included in delay calculation
-    delay = (timeframe - (TFtop_pauseTime))/(total_steps*2)  #Example 2sec/(200steps*2) = 0.005 time between each step  
+    #0.8 to make the delay slightly shorter than exact timeframe, for leeway during time reads
+    delay = 0.8*(timeframe - TFtop_pauseTime)/(total_steps*4)  #Example 2sec/(200steps*2) = 0.005 time between each step  
     
     #Used to find current steps position to rotate to, based on time in the day
-    step_constant = ((timeframe - (TFtop_pauseTime))/2)*total_steps
+    step_constant = (2/(timeframe - TFtop_pauseTime))*total_steps
+    
+    printed = False
+    print("Begin")
     
     while True:
         currTime = time.localtime()
@@ -73,34 +79,44 @@ def dayProcess(timeFactor, operating_daytime, top_pauseTime, total_steps, daySta
         dayBegEpoch = time.mktime((year,month,day,0,0,0,wkday,yrday))
         currEpoch = time.mktime(currTime)
         currDay = currEpoch - dayBegEpoch
-        print("currDay: ", currDay)
+        #print("currDay: ", currDay)
         
         #Example: currDay is 3hr:7min:0sec which is 11220 seconds, TFday is 30min=1800sec
         #TFcurrDay is 420sec=7min into the timeframe
-        TFcurrDay = (currDay/timeFactor) % TFday
-        print("TFcurrDay: ", TFcurrDay)
+        TFcurrDay = currDay % TFday
         
-        if TFcurrDay > TFdayStart and TFcurrDay< TFdayEnd:
-            if TFcurrDay < TFdayStart + (timeframe - TFtop_pauseTime)/2:
-                targetSteps = floor(TFcurrDay * step_constant)
-                print("yo")
-            elif TFcurrDay > TFdayStart + (timeframe + TFtop_pauseTime)/2:
-                targetSteps = floor(total_steps - TFcurrDay*step_constant)
-                print("ba")
+        if TFcurrDay >= TFdayStart and TFcurrDay <= TFdayEnd:
+            if TFcurrDay <= TFdayStart + (timeframe - TFtop_pauseTime)/2:
+                targetSteps = floor((TFcurrDay - TFdayStart) * step_constant)
+#                 print("yo")
+#                 print("targetSteps: ", targetSteps)
+            elif TFcurrDay >= TFdayStart + (timeframe + TFtop_pauseTime)/2:
+                targetSteps = floor(total_steps - (TFcurrDay - (TFdayStart + (timeframe + TFtop_pauseTime)/2))*step_constant)
+#                 print("ba")
+#                 print("targetSteps: ", targetSteps)
+
         #Prevent motor from rotating past structure limits    
         if targetSteps < 0 or targetSteps > total_steps:
             raise Exception("targetSteps out of range: ", targetSteps)
         
-        if targetSteps > 0 and targetSteps < total_steps:
+        if targetSteps >= 0 and targetSteps <= total_steps:
             if currSteps < targetSteps:
                 currSteps = rotateMotor(targetSteps - currSteps, CW, delay, currSteps)
             elif currSteps > targetSteps:
                 currSteps = rotateMotor(currSteps - targetSteps, CCW, delay, currSteps)
         
-            print("targetSteps: ", targetSteps)
-            print("currSteps: ", currSteps)
+        #Only print every 10 seconds, excessive printing slows program down
+        if TFcurrDay % 3 == 0:
+            if printed == False:
+                print("TFcurrDay: ", TFcurrDay)
+                print("targetSteps: ", targetSteps)
+                print("currSteps: ", currSteps)
+                printed = True
+        else:
+            printed = False
+                
             
-        time.sleep(4)
+        time.sleep(0.001)
         
 if __name__ == "__main__":
     calibrateRTC()
